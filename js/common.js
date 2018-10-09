fec4cb40c6db534cc8e51a0a0aaf3203e7430144
fun <lljxx1@gmail.com>
2018-10-09 19:18:01 +0800

    merge vote support

--- js/common.js
+++ js/common.js
@@ -791,6 +791,7 @@ var VoterDetail = {
     },
 
     mounted: function () {
+        
         // this.eosClient = Eos({
         //     chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
         //     httpEndpoint: 'https://api.eosmedi.com',
@@ -811,6 +812,10 @@ var VoterDetail = {
             }
         },
         viewBlock: function(block, voteAction, index){
+            if(!this.eosClient){
+                return;
+            }
+
             var self = this;
             if(self.querying[block]){
                 return;
